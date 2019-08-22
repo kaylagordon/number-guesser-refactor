e0e4ebeda149717bc2e5774f06ddf899a9fe07d0
Kayla Wood <kaylaewood@gmail.com>
2019-08-22 13:38:43 -0600

    Add up to date ids and js

--- main.js
+++ main.js
@@ -1,11 +1,19 @@
+//Changes text to display new range
 var minRangeInput = document.getElementById("min-range-input");
 var maxRangeInput = document.getElementById("max-range-input");
 var minRangeText = document.getElementById("min-range");
 var maxRangeText = document.getElementById("max-range");
-var updateButton = document.querySelector("#update-button");
+var updateButton = document.getElementById("update-button");
 
 
 updateButton.addEventListener("click", function() {
   minRangeText.innerText = minRangeInput.value;
   maxRangeText.innerText = maxRangeInput.value;
 });
+
+//When "submit" is clicked, only alpha-numeric/numeric is accepted
+var player1NameInput = document.getElementById("player1-name-textbox");
+var player2NameInput = document.getElementById("player2-name-textbox");
+var player1GuessInput = document.getElementById("player1-guess-textbox");
+var player2GuessInput = document.getElementById("player2-guess-textbox");
+var submitGuessButton = document.getElementById("submit-guess-button");
